839ba656ec1612557dd150b88e33d34907c4e8f0
finncoolcoder <164394858+finncoolcoder@users.noreply.github.com>
2024-08-06 21:19:43 -0400

    Update game.py

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ def menu():
     print('MENU:')
     print(' 1: create a new save. \n 2: Load an existing save\n 3:About, copyright etc. (boring stuff) \n 4: THe debug version')
 
-    
+    #all this shit is broken!! however it works for my very specific use case.
 menu()
 option1=input('\n >')
 
@@ -29,3 +29,22 @@ else:
     menu()
 
 print('you have passed')
+
+print('Welcome to stardew valley.')
+print('please create a character.')
+name = input('what is your name >')
+age = int(input('how old are you?'))
+favoritething = input('What is your favorite thing')
+pet12 = input('1 for cat 2 for dog')
+if pet12 == 1:
+    pet='cat'
+if pet12 == 2:
+    pet='dog'
+else:
+    pet=input('what pet??/?????????>')
+
+
+print('this is your information, no you cannot change it.')
+print(name + str(age) + favoritething + pet)
+
+tprint("let's go!")
