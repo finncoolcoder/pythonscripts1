cf07f0282ba0abcc79a10665bb47c1abdf2806ae
finncoolcoder <164394858+finncoolcoder@users.noreply.github.com>
2024-08-07 12:08:50 -0400

    Test

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import random
 import time
+import webbrowser
 from art import *
 print('put this window in fullscreen')
 x1=5
@@ -20,7 +21,7 @@ def menu():
     print('MENU:')
     print(' 1: create a new save. \n 2: Load an existing save\n 3:About, copyright etc. (boring stuff) \n 4: THe debug version')
 
-    #all this shit is broken!! however it works for my very specific use case.
+    print('all this shit is broken!! however it works for my very specific use case.')
 menu()
 option1=input('\n >')
 
@@ -59,19 +60,127 @@ time.sleep(2)
 print('ok seriously though lets start.')
 print('You (yes you) are a farmer. \n you must make money.')
 print('run the command /farm to get started.')
-input = input('>')
+ask2 = input('>')
+
+
+
+import random
+import time
+import webbrowser
+from art import *
+print('put this window in fullscreen')
+x1=5
+print(str(x1))
+time.sleep(1)
+print(str(x1-1))
+time.sleep(1)
+print(str(x1-2))
+time.sleep(1)
+print(str(x1-3))
+time.sleep(1)
+print(str(x1-4))
+time.sleep(1)
+
+tprint('Stardew Valley \n text edtion')
+time.sleep(2)
+def menu():
+    print('MENU:')
+    print(' 1: create a new save. \n 2: Load an existing save\n 3:About, copyright etc. (boring stuff) \n 4: THe debug version')
+
+    print('all this shit is broken!! however it works for my very specific use case.')
+menu()
+option1=input('\n >')
+
+if option1 == '4':
+    pass
+else:
+    menu()
+
+print('you have passed')
+
+print('Welcome to stardew valley.')
+print('please create a character.')
+name = input('what is your name >')
+age = int(input('how old are you?'))
+favoritething = input('What is your favorite thing')
+pet12 = input('1 for cat 2 for dog')
+if pet12 == 1:
+    pet='cat'
+if pet12 == 2:
+    pet='dog'
+else:
+    pet=input('what pet??/?????????>')
+
+
+print('this is your information, no you cannot change it.')
+print(name + str(age) + favoritething + pet)
+
+tprint("let's go!")
+time.sleep(2)
+print('you are about to start a bad quality port of a really good game. ')
+print('stardew valley the text edition , like the original was created by a solo developer.')
+time.sleep(2)
+print('If you would like to donate please send me an email and I can reccive cash in the mail.')
+print('Just send a photo of your passport along with your SSN and credit card info.')
+time.sleep(2)
+print('ok seriously though lets start.')
+print('You (yes you) are a farmer. \n you must make money.')
+print('run the command /farm to get started.')
+ask2 = input('>')
+
+
+
 def farm():
     print('you are in farming mode')
-    turnips=0
+    
+    global turnips, turnipspersecond
+    turnips = 0
     turnipspersecond=turnips/4
-    print('you have '+turnips+' turnips, press any key and then enter to plant turnips.')
-    input('')
+    print('you have '+str(turnips)+' turnips, press any key and then enter to plant turnips.')
+    input('>')
     time.sleep(random.randint(1,20))
     turnips=turnips+random.randint(1,300)
     turnipspersecond=turnips/4
-    print('you now have '+ turnips+ ' producing a total of ' + turnipspersecond + ' turnips every second.')
+    print('you now have ' + str(turnips) + ' turnips. producing a total of ' + str(turnipspersecond) + ' turnips every second.')
 
-if input == '/farm':
+
+
+
+
+if ask2 == '/farm':
     farm()
 else:
     farm()
+while True:
+    turnips=turnipspersecond
+
+    turnipspersecond=turnips/4
+    print('turnips' + str(turnips))
+    time.sleep(0.9)
+
+
+
+
+print('you are in the main program.')
+
+
+
+
+
+
+if ask2 == '/farm':
+    farm()
+else:
+    farm()
+while True:
+    turnips=turnipspersecond
+
+    turnipspersecond=turnips/4
+    print('turnips' + str(turnips))
+    time.sleep(0.9)
+
+
+
+
+print('you are in the main program.')
+
---
+++ gamefixed.py
@@ -0,0 +1,89 @@
+import random
+import time
+import webbrowser
+from art import tprint  # Ensure `tprint` is imported from `art`
+
+print('Put this window in fullscreen')
+x1 = 5
+for i in range(5):
+    print(str(x1 - i))
+    time.sleep(1)
+
+tprint('Stardew Valley \n Text Edition')
+time.sleep(2)
+
+def menu():
+    print('MENU:')
+    print('X1: Create a new save.')
+    print('2: Load an existing save')
+    print('3: About, copyright etc. (boring stuff)')
+    print('4: The debug version')
+
+def handle_menu():
+    while True:
+        menu()
+        option = input('\n> ')
+        if option == '4':
+            break  # Exit the loop if option 4 is selected
+
+handle_menu()
+
+print('You have passed')
+print('Welcome to Stardew Valley.')
+print('Please create a character.')
+
+name = input('What is your name? > ')
+age = int(input('How old are you? '))
+favorite_thing = input('What is your favorite thing? ')
+pet12 = int(input('1 for cat, 2 for dog > '))
+
+if pet12 == 1:
+    pet = 'cat'
+elif pet12 == 2:
+    pet = 'dog'
+else:
+    pet = input('What pet? > ')
+
+print('This is your information; no, you cannot change it.')
+print(f'Name: {name}, Age: {age}, Favorite Thing: {favorite_thing}, Pet: {pet}')
+
+tprint("Let's go!")
+time.sleep(2)
+
+print('You are about to start a bad quality port of a really good game.')
+print('Stardew Valley: The Text Edition, like the original, was created by a solo developer.')
+time.sleep(2)
+print('If you would like to donate, please send me an email.')
+print('Just send a photo of your passport along with your SSN and credit card info.')
+time.sleep(2)
+print('Okay seriously though, let\'s start.')
+print('You (yes you) are a farmer. \nYou must make money.')
+print('Run the command /farm to get started.')
+
+ask2 = input('> ')
+
+def farm():
+    global turnips, turnips_per_second
+    turnips = 0
+    turnips_per_second = turnips / 4
+    print(f'You have {turnips} turnips. Press any key and then enter to plant turnips.')
+    input('>')
+    time.sleep(random.randint(1, 20))
+    turnips += random.randint(1, 300)
+    turnips_per_second = turnips / 4
+    print(f'You now have {turnips} turnips, producing a total of {turnips_per_second} turnips every second.')
+
+if ask2 == '/farm':
+    farm()
+else:
+    farm()
+
+# Run farming simulation in a loop
+try:
+    while True:
+        turnips += turnips_per_second^age
+        turnips_per_second = turnips / 4
+        print(f'Turnips: {turnips}')
+        time.sleep(0.9)
+except KeyboardInterrupt:
+    print('Exiting the farming simulation.')
