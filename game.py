ce8d0123d0cda177598d62fced9db35bf6d63402
finncoolcoder <164394858+finncoolcoder@users.noreply.github.com>
2024-08-06 21:08:08 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,31 @@
 import random
+import time
 from art import *
-tprint('heyy')
+print('put this window in fullscreen')
+x1=5
+print(str(x1))
+#time.sleep(1)
+print(str(x1-1))
+#time.sleep(1)
+print(str(x1-2))
+#time.sleep(1)
+print(str(x1-3))
+#time.sleep(1)
+print(str(x1-4))
+#time.sleep(1)
+
+tprint('Stardew Valley \n text edtion')
+def menu():
+    print('MENU:')
+    print(' 1: create a new save. \n 2: Load an existing save\n 3:About, copyright etc. (boring stuff) \n 4: THe debug version')
+
+    
+menu()
+option1=input('\n >')
+
+if option1 == '4':
+    pass
+else:
+    menu()
+
+print('you have passed')
