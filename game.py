18ed5f379190495dbee311137afc65e9e8c3670a
finncoolcoder <164394858+finncoolcoder@users.noreply.github.com>
2024-08-06 21:28:52 -0400

    remove comments on time sleeps

--- game.py
+++ game.py
@@ -4,17 +4,18 @@ from art import *
 print('put this window in fullscreen')
 x1=5
 print(str(x1))
-#time.sleep(1)
+time.sleep(1)
 print(str(x1-1))
-#time.sleep(1)
+time.sleep(1)
 print(str(x1-2))
-#time.sleep(1)
+time.sleep(1)
 print(str(x1-3))
-#time.sleep(1)
+time.sleep(1)
 print(str(x1-4))
-#time.sleep(1)
+time.sleep(1)
 
 tprint('Stardew Valley \n text edtion')
+time.sleep(2)
 def menu():
     print('MENU:')
     print(' 1: create a new save. \n 2: Load an existing save\n 3:About, copyright etc. (boring stuff) \n 4: THe debug version')
@@ -48,3 +49,8 @@ print('this is your information, no you cannot change it.')
 print(name + str(age) + favoritething + pet)
 
 tprint("let's go!")
+
+print('you are about to start a bad quality port of a really good game. ')
+print('stardew valley the text edition , like the original was created by a solo developer.')
+print('If you would like to donate please send me an email and I can reccive cash in the mail.')
+print('Just send a photo of your passport along with your SSN and credit card info.')
