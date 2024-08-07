c0f5fa8aad6c2124927018c4d196d03147a88299
finncoolcoder <164394858+finncoolcoder@users.noreply.github.com>
2024-08-06 20:27:37 -0400

    add comments

--- workingfiles.py
+++ workingfiles.py
@@ -3,8 +3,8 @@ print('press 1 then enter to open the book')
 book=input('\n')
 if book=='1':
     print('book was = to 1 you are now inside a block')
-    file = open('C:/Users/ltidd/Dropbox/My PC (LAPTOP-S3NMNLN1)/Documents/list.txt', 'r')
-
+    file = open('C:/Users/ltidd/Dropbox/My PC (LAPTOP-S3NMNLN1)/Documents/list.txt', 'r') #this is the path to the file you want to read.
+    #make sure the path uses forward slashes even if you are on windows :P
     # This will print every line one by one in the file
     for each in file:
         print (each)
